1a447d9b27d965e38b54be50d54dc2b1b242f490
Jacob Moeller <jacobmoe@gmail.com>
2014-12-03 23:11:32 -0500

    user model

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 var express = require('express')
+var mongoose = require('mongoose');
 var app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-var server = app.listen(3000, function () {
+app.set('port', process.env.PORT || 3000)
 
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('listening at http://%s:%s', host, port)
-
+var server = app.listen(app.get('port'), function () {
+  console.log('listening on port', server.address().port)
 })
+
+mongoose.connection.on('connected', function(ref) {
+  console.log('connected to mongo')
+})
+
+var nodeEnv = process.env.NODE_ENV || 'development'
+var dbName = "tuneyard-" + nodeEnv
+
+mongoose.connect('mongodb://localhost/' + dbName)
