7b5524f17f2fa50ec52550d147a35f2f3e3d8b9f
Jacob Moeller <jacobmoe@gmail.com>
2014-12-04 22:06:28 -0500

    test user validations

--- test/backend/lib/models/user.js
+++ test/backend/lib/models/user.js
@@ -1,8 +1,19 @@
+var User = require('../../../../lib/models/user')
+
 describe('Model: User', function () {
 
-  it('works', function (done) {
-    assert(true)
-    done()
+  describe('validations', function () {
+    it('requires email, username, hash and salt', function (done) {
+      User.create({}, function (err, user) {
+        assert.ok(err)
+        assert.notOk(user)
+        assert.equal('ValidatorError', err.errors.email.name)
+        assert.equal('ValidatorError', err.errors.username.name)
+        assert.equal('ValidatorError', err.errors.hash.name)
+        assert.equal('ValidatorError', err.errors.salt.name)
+        done()
+      })
+    })
   })
 
 })
