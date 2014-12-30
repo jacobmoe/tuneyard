081a237ebcfe99d02a56bd3fe408f6e09ca010bb
Jacob Moeller <jacobmoe@gmail.com>
2014-12-30 17:28:11 -0500

    setPassword method on account

--- test/backend/lib/models/account.js
+++ test/backend/lib/models/account.js
@@ -1,8 +1,11 @@
+var bcrypt = require('bcrypt')
+
 var Account = require('../../../../lib/models/account')
 
 describe('Model: Account', function () {
 
   before(connectDb)
+  beforeEach(clearCollections)
 
   describe('validations', function () {
     it('requires email, hash and salt', function (done) {
@@ -16,23 +19,70 @@ describe('Model: Account', function () {
       })
     })
   })
+  
+  describe('#setPassword', function () {
+    var password = 'test@example.com'
 
-  describe('methods', function () {
+    it('encrypts and sets the password', function (done) {
+      var account = new Account({email: 'test@example.com'})
+      account.setPassword(password, password, function (err) {
+        assert.isNull(err)
 
-    describe('validPassword', function () {
+        assert.ok(account.hash)
+        assert.ok(account.salt)
+        
+        assert.notEqual(password, account.hash)
 
-      it('compares given password with actual password', function (done) {
-        var account = new Account({email: 'test@example.com'})
-        account.setPassword('thepassword', 'thepassword', function (err) {
-          account.save(function () {
-            console.log("------------", err)
-            done()
-          })
+        bcrypt.compare(password, account.hash, function(err, res) {
+          assert.notOk(err)
+          assert.isTrue(res)
+          done()
         })
-      })
 
+      })
     })
 
+    it('returns an error when password does not match confirmation', function (done) {
+      var account = new Account({email: 'test@example.com'})
+      account.setPassword(password, 'notthepassword', function (err) {
+        assert.ok(err)
+        assert.isObject(err)
+
+        assert.isNull(account.hash)
+        assert.isNull(account.salt)
+        
+        done()
+      })
+    })
+    
   })
+
+  describe('#validPassword', function () {
+    var account, password = 'test@example.com'
+
+    beforeEach(function (done) {
+      var a = new Account({email: 'test@example.com'})
+      a.setPassword(password, password, function (err) {
+        a.save(function (err) {
+          assert.isNull(err)
+          account = a
+          done()
+        })
+      })
+    })
+
+    it('compares given password with actual password', function (done) {
+      var isValid = account.validPassword(password)
+
+      assert.isTrue(isValid)
+
+      isValid = account.validPassword('nope')
+
+      assert.isFalse(isValid)
+
+      done()
+    })
+  })
+  
 
 })
--- test/helper.js
+++ test/helper.js
@@ -1,5 +1,7 @@
 process.env.NODE_ENV = 'test'
 
+var async = require('async')
+
 var chai = require('chai')
 var chaiAsPromised = require('chai-as-promised')
 var Q = require('q')
@@ -16,6 +18,19 @@ global.Assertion = chai.Assertion
 global.assert = chai.assert
 
 global.defer = Q.defer
+
+global.clearCollections = function (done) {
+  var jobs = []
+  var collections = ['account']
+  
+  collections.forEach(function (col) {
+    jobs.push(function (cb) {
+      require('../lib/models/' + col).remove({}, cb)
+    })
+  })
+  
+  async.parallel(jobs, done)
+} 
 
 global.connectDb = function(done) {
   require('../lib/db').connect(done)
