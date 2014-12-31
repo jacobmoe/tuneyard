edc0774aa30ca095b0e7167e951b13c12ebdc303
Jacob Moeller <jacobmoe@gmail.com>
2014-12-31 11:56:50 -0500

    lock down accounts api

--- test/routers/api/accounts.js
+++ test/routers/api/accounts.js
@@ -1,29 +1,79 @@
 var request = require('supertest')
   , app = require('express')()
-  , bodyParser = require('body-parser')
 
-var router = require('../../../lib/routers/api/accounts')
+var routes = require('../../../lib/routes')
+  , middleware = require('../../../lib/middleware')
+  , Account = require('../../../lib/models/account')
 
-app.use(bodyParser.json())
-app.use('/api/accounts', router)
+middleware(app)
+routes(app)
 
 describe('router: accounts', function () {
+  var account
 
   before(connectDb)
   beforeEach(clearCollections)
 
+  beforeEach(function (done) {
+    var params = {
+      email: 'test@example.com',
+      password: 'thepassword',
+      passwordConfirmation: 'thepassword'
+    }
+    Account.create(params, function (err, a) {
+      account = a
+
+      done()
+    })
+  })
+
   describe('GET /', function () {
-    it('returns a collection of accounts', function (done) {
-      request(app)
-        .get('/api/accounts')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end(function(err, res) {
-          assert.isArray(res.body)
-          done()
-        })
+    context('authorized', function () {
+      var agent
+
+      beforeEach(function (done) {
+        agent = request.agent(app)
+
+        agent
+          .post('/api/sessions')
+          .send({
+            email: account.email,
+            password: 'thepassword'
+          })
+          .expect(200)
+          .end(done)
       })
+
+      it('returns a collection of accounts', function (done) {
+        agent
+          .get('/api/accounts')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            assert.isNull(err)
+            assert.isArray(res.body)
+            done()
+          })
+      })
+    })
+  })
+
+  describe('GET /', function () {
+    context('not authorized', function () {
+      it('returns a collection of accounts', function (done) {
+        request(app)
+          .get('/api/accounts')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(403)
+          .end(function(err, res) {
+            assert.isNull(err)
+            assert.equal(res.body.message, 'must be logged in')
+            done()
+          })
+      })
+    })
   })
 
 })
