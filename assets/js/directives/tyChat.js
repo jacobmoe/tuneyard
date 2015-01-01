e801191936148a541846665f3bdbfdb58b044da8
Jacob Moeller <jacobmoe@gmail.com>
2015-01-01 01:03:20 -0500

    chatBox

--- assets/js/directives/tyChat.js
+++ assets/js/directives/tyChat.js
@@ -1,30 +1,42 @@
-angular.module('tuneyard').directive('tyChat', function() {
+angular.module('tuneyard').directive('tyChat', function(socket, $rootScope) {
   return {
     restrict: 'E',
     replace: true,
     templateUrl: '/assets/templates/directives/ty-chat.html',
-    controller: ['$scope', '$rootScope', 'socket', function($scope, $rootScope, socket) {
-      $scope.currentMessage = ''
-      $scope.messages = []
+    link: function(scope, element) {
+      scope.currentMessage = ''
+      scope.messages = []
       
-      $scope.newMessage = function () {
+      scope.newMessage = function () {
         socket.emit('add-new-message', {
-          content: $scope.currentMessage,
+          content: scope.currentMessage,
           account: $rootScope.currentUser._id
         })
-        $scope.currentMessage = ''
+        scope.currentMessage = ''
+      }
+      
+      scope.isLast = function (index) {
+        return index === scope.messages.length - 1
       }
       
       socket.on('new-message', function (data) {
-        $scope.messages.push(data)
+        scope.messages.push(data)
       })
       
       socket.on('init-messages', function (data) {
-        $scope.messages = _.sortBy(data, function (m) {
+        scope.messages = _.sortBy(data, function (m) {
           return new Date(m.createdAt)
         })
       })
 
-    }]
+      scope.$watchCollection('messages', function () {
+        var chatBox = element.find('.chatBox')[0]
+        if (!chatBox) return
+
+        chatBox.scrollTop = chatBox.scrollHeight
+      })
+
+      socket.emit('init-chat')
+    }
   }
 })
