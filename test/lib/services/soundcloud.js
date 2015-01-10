c938eba5531517ea48255177a21e40f32ba3bc0b
Jacob Moeller <jacobmoe@gmail.com>
2015-01-10 17:43:10 -0500

    source parser method for soundcloud

--- test/lib/services/soundcloud.js
+++ test/lib/services/soundcloud.js
@@ -21,7 +21,7 @@ describe('service: soundcloud', function () {
       .reply(200, expected)
     })
 
-    it('works', function (done) {
+    it('returns track data', function (done) {
 
       service.getTrackData(url, function (err, data) {
         assert.deepEqual(data, {
@@ -31,6 +31,37 @@ describe('service: soundcloud', function () {
         })
         done()
       })
+    })
+  })
+
+  describe('isSoundcloudUrl', function () {
+
+    it('checks if url is valid soundcloud url', function (done) {
+      var url = "https://soundcloud.com/asthmatickitty/sufjan-stevens-silver-gold"
+      assert.isTrue(service.isSoundcloudUrl(url))
+
+      url = "https://soundcloud.com/musikedidiable/handclaps"
+      assert.isTrue(service.isSoundcloudUrl(url))
+
+      url = "https://snd.sc/bluesoapmusic/brown-eyes-the-new-southern-electrikk"
+      assert.isFalse(service.isSoundcloudUrl(url))
+
+      url = "https://soundcloud.com/subpop"
+      assert.isFalse(service.isSoundcloudUrl(url))
+
+      url = "https://roundmound.com/bluesoapmusic/brown-eyes-the-new-southern-electrikk"
+      assert.isFalse(service.isSoundcloudUrl(url))
+
+      url = "http://www.nytimes.com/"
+      assert.isFalse(service.isSoundcloudUrl(url))
+
+      url = "http://www.nytimes.com/"
+      assert.isFalse(service.isSoundcloudUrl(url))
+
+      url = "not a url"
+      assert.isFalse(service.isSoundcloudUrl(url))
+
+      done()
     })
   })
 
